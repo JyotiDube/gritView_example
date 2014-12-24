4bbf4c92a430adbd9c16d8b9ca79a6c8275a4157
JyotiDube <j.dube311@gmail.com>
2014-12-24 15:49:37 +0530

    Display image in GridView

--- GridViewApp/src/com/example/gridviewapp2/MainActivity.java
+++ GridViewApp/src/com/example/gridviewapp2/MainActivity.java
@@ -3,15 +3,23 @@ package com.example.gridviewapp2;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.GridView;
 
 public class MainActivity extends Activity {
 
+	
+	GridView gridview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        
+        gridview =(GridView) findViewById(R.id.grid_View);
+        gridview.setAdapter(new ImageAdapter(MainActivity.this));
 
+    }
+    
+    
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
